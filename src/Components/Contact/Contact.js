cda13b8d9b3ff150ea5d359ceda1e5f221c4671e
Krystian Pietroń <krystianpietr@gmail.com>
2019-03-15 12:52:39 +0100

    add components Contact

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Contact = props => (
     <section>
-
+        <ul>
+            <li><a>Tel. 722 366 109</a></li>
+            <li><a mailto="">Email: krystianpietr@gmail.com</a></li>
+            <li><a href=" "><img src="" /></a></li>
+            <li><a href=" "><img src="" /></a></li>
+            <li><a href=" "><img src="" /></a></li>
+        </ul>
     </section>
 )
 
