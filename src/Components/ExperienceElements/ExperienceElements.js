2e3f2000650a79fca3dda2fd11d6fa4d13ab31f7
Krystian Pietroń <krystianpietr@gmail.com>
2019-03-16 13:29:37 +0100

    add paragraph to ExperienceElements.js

---
+++ src/Components/ExperienceElements/ExperienceElements.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const ExperienceElements = props => (
+    <section>
+        <h3>Doświadczenie</h3>
+        <div>
+            <h3>Kurs Front-End Developer</h3>
+            <a href="https://infoshareacademy.com/">infoShare Academy Lublin</a>
+            <p>Październik 2018 - Grudzień 2018</p>
+            <p>
+                Kurs trwał 320 godzin. Na kursie poznałem narzędzia<br />
+                programistyczne tj. : IDE , GIT i GitHub oraz Jira. Nauczyłem się<br />
+                używac systemu kontroli wersji GIT. Poznałem znaczniki HTML oraz<br />
+                CSS. Potrafię programować w JavaScript dynamiczne elementy<br />
+                stron www jak i tworzyć gry oraz aplikacje. Poznałem obsługę<br />
+                biblioteki REACT podczas tworzenia aplikacji webowych. Potrafię<br />
+                pracować w zespole SCRUM. Podczas kursu wraz z kolegami<br />
+                stworzyliśmy stronę www wraz z aplikacją webową TwojaKaloria do<br />
+                zliczania dziennego spożycia kalorii oraz innych makroelementów w<br />
+                posiłkach. Dzięki aplikacji jesteśmy też w stanie zaplanować sobie<br />
+                posiłkina dane dni.
+                </p>
+        </div>
+    </section>
+)
+
+export default ExperienceElements
